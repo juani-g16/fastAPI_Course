46c4ec3a52dd44c82baf24c56d9e82073cc382e0
Juan Giorgetti <juan.giorgetti@itk-engineering.com>
2024-08-02 16:06:06 +0200

    Added tatabase migration with alembic library. Small code cleanup.

---
+++ alembic/versions/a71fe689ef65_add_foreign_key_to_post_table.py
@@ -0,0 +1,39 @@
+"""add foreign key to post table
+
+Revision ID: a71fe689ef65
+Revises: 9b55f1a016bd
+Create Date: 2024-08-02 14:19:04.975926
+
+"""
+
+from threading import local
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "a71fe689ef65"
+down_revision: Union[str, None] = "9b55f1a016bd"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("posts", sa.Column("owner_id", sa.Integer(), nullable=False))
+    op.create_foreign_key(
+        "post_users_fk",
+        source_table="posts",
+        referent_table="users",
+        local_cols=["owner_id"],
+        remote_cols=["id"],
+        ondelete="CASCADE",
+    )
+    pass
+
+
+def downgrade() -> None:
+    op.drop_constraint("post_users_fk", table_name="posts")
+    op.drop_column("posts", "owner_id")
+    pass
--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ from .database import engine
 from .routers import post, user, auth, vote
 from .config import settings
 
-models.Base.metadata.create_all(bind=engine)
+# with Alembic module this is not longer necesary (althoug it could be kept without problems)
+# models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
